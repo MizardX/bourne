0bccb08cd2339ef45fc00cf84e312a26dd7db6e7
Markus Jarderot <mizardx@gmail.com>
2024-12-31 00:09:50 +0100

    Add JSON.org test cases

--- src/lib.rs
+++ src/lib.rs
@@ -360,4 +360,226 @@ mod tests {
         assert_eq!(json_text, r#"{"int":9223372036854775807,"float":3.14159265358979}"#);
         Ok(())
     }
+
+    #[test]
+    fn parse_pass_1() -> Result<(), crate::error::ParseError> {
+        let _ = Value::from_str(include_str!("../tests/pass1.json"))?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_pass_2() -> Result<(), crate::error::ParseError> {
+        let _ = Value::from_str(include_str!("../tests/pass2.json"))?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_pass_3() -> Result<(), crate::error::ParseError> {
+        let _ = Value::from_str(include_str!("../tests/pass3.json"))?;
+        Ok(())
+    }
+
+    #[test]
+    #[ignore = "This check is not yet implemented."]
+    fn test_fail_1() {
+        let result = Value::from_str(include_str!("../tests/fail1.json"));
+        assert!(result.is_err(), "A JSON payload should be an object or array, not a string.");
+    }
+
+    #[test]
+    fn test_fail_2() {
+        let result = Value::from_str(include_str!("../tests/fail2.json"));
+        assert!(result.is_err(), "Unclosed array");
+    }
+
+    #[test]
+    fn test_fail_3() {
+        let result = Value::from_str(include_str!("../tests/fail3.json"));
+        assert!(result.is_err(), "keys must be quoted");
+    }
+
+    #[test]
+    fn test_fail_4() {
+        let result = Value::from_str(include_str!("../tests/fail4.json"));
+        assert!(result.is_err(), "extra comma");
+    }
+
+    #[test]
+    fn test_fail_5() {
+        let result = Value::from_str(include_str!("../tests/fail5.json"));
+        assert!(result.is_err(), "double extra comma");
+    }
+
+    #[test]
+    fn test_fail_6() {
+        let result = Value::from_str(include_str!("../tests/fail6.json"));
+        assert!(result.is_err(), "missing value before comma");
+    }
+
+    #[test]
+    fn test_fail_7() {
+        let result = Value::from_str(include_str!("../tests/fail7.json"));
+        assert!(result.is_err(), "Comma after the close");
+    }
+
+    #[test]
+    fn test_fail_8() {
+        let result = Value::from_str(include_str!("../tests/fail8.json"));
+        assert!(result.is_err(), "Extra close");
+    }
+
+    #[test]
+    fn test_fail_9() {
+        let result = Value::from_str(include_str!("../tests/fail9.json"));
+        assert!(result.is_err(), "Extra comma");
+    }
+
+    #[test]
+    fn test_fail_10() {
+        let result = Value::from_str(include_str!("../tests/fail10.json"));
+        assert!(result.is_err(), "Extra value after close");
+    }
+
+    #[test]
+    fn test_fail_11() {
+        let result = Value::from_str(include_str!("../tests/fail11.json"));
+        assert!(result.is_err(), "Illegal expression");
+    }
+
+    #[test]
+    fn test_fail_12() {
+        let result = Value::from_str(include_str!("../tests/fail12.json"));
+        assert!(result.is_err(), "Illegal invocation");
+    }
+
+    #[test]
+    fn test_fail_13() {
+        let result = Value::from_str(include_str!("../tests/fail13.json"));
+        assert!(result.is_err(), "Numbers cannot have leading zeroes");
+    }
+
+    #[test]
+    fn test_fail_14() {
+        let result = Value::from_str(include_str!("../tests/fail14.json"));
+        assert!(result.is_err(), "Numbers cannot be hex");
+    }
+
+    #[test]
+    #[ignore = "This is explicitly allowed by this parser"]
+    fn test_fail_15() {
+        let result = Value::from_str(include_str!("../tests/fail15.json"));
+        assert!(result.is_err(), "Illegal backslash escape");
+    }
+
+    #[test]
+    fn test_fail_16() {
+        let result = Value::from_str(include_str!("../tests/fail16.json"));
+        assert!(result.is_err(), "Text without quotes");
+    }
+
+    #[test]
+    #[ignore = "This is explicitly allowed by this parser"]
+    fn test_fail_17() {
+        let result = Value::from_str(include_str!("../tests/fail17.json"));
+        assert!(result.is_err(), "Illegal backslash escape");
+    }
+
+    #[test]
+    #[ignore = "Need to be able to specify the maximum depth"]
+    fn test_fail_18() {
+        let result = Value::from_str(include_str!("../tests/fail18.json"));
+        assert!(result.is_err(), "Too deep");
+    }
+
+    #[test]
+    fn test_fail_19() {
+        let result = Value::from_str(include_str!("../tests/fail19.json"));
+        assert!(result.is_err(), "Missing colon");
+    }
+
+    #[test]
+    fn test_fail_20() {
+        let result = Value::from_str(include_str!("../tests/fail20.json"));
+        assert!(result.is_err(), "Double colon");
+    }
+
+    #[test]
+    fn test_fail_21() {
+        let result = Value::from_str(include_str!("../tests/fail21.json"));
+        assert!(result.is_err(), "Comma instead of colon");
+    }
+
+    #[test]
+    fn test_fail_22() {
+        let result = Value::from_str(include_str!("../tests/fail22.json"));
+        assert!(result.is_err(), "Colon instead of comma");
+    }
+
+    #[test]
+    fn test_fail_23() {
+        let result = Value::from_str(include_str!("../tests/fail23.json"));
+        assert!(result.is_err(), "Bad value");
+    }
+
+    #[test]
+    fn test_fail_24() {
+        let result = Value::from_str(include_str!("../tests/fail24.json"));
+        assert!(result.is_err(), "Single quote");
+    }
+
+    #[test]
+    #[ignore = "This is explicitly allowed by this parser"]
+    fn test_fail_25() {
+        let result = Value::from_str(include_str!("../tests/fail25.json"));
+        assert!(result.is_err(), "Tab character in string");
+    }
+
+    #[test]
+    #[ignore = "This is explicitly allowed by this parser"]
+    fn test_fail_26() {
+        let result = Value::from_str(include_str!("../tests/fail26.json"));
+        assert!(result.is_err(), "Escaped tab character in string");
+    }
+
+    #[test]
+    fn test_fail_27() {
+        let result = Value::from_str(include_str!("../tests/fail27.json"));
+        assert!(result.is_err(), "Newline in string");
+    }
+
+    #[test]
+    fn test_fail_28() {
+        let result = Value::from_str(include_str!("../tests/fail28.json"));
+        assert!(result.is_err(), "Escaped newline in string");
+    }
+
+    #[test]
+    fn test_fail_29() {
+        let result = Value::from_str(include_str!("../tests/fail29.json"));
+        assert!(result.is_err(), "Unexpected end of number");
+    }
+
+    #[test]
+    fn test_fail_30() {
+        let result = Value::from_str(include_str!("../tests/fail30.json"));
+        assert!(result.is_err(), "Unexpected end of number");
+    }
+
+    #[test]
+    fn test_fail_31() {
+        let result = Value::from_str(include_str!("../tests/fail31.json"));
+        assert!(result.is_err(), "Double signed exponent");
+    }
+
+    #[test]
+    fn test_fail_32() {
+        let result = Value::from_str(include_str!("../tests/fail32.json"));
+        assert!(result.is_err(), "Comma instead of close curly brace");
+    }
+
+    #[test]
+    fn test_fail_33() {
+        let result = Value::from_str(include_str!("../tests/fail33.json"));
+        assert!(result.is_err(), "Mismatched close curly brace");
+    }
 }
